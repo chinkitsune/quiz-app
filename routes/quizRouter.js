5e5928fcb3de599d121d7c7777598b4ec40db26b
chen jiaying <chen0813@algonquinlive.com>
2026-05-05 13:00:37 -0400

    before generated quiz pug

--- routes/quizRouter.js
+++ routes/quizRouter.js
@@ -7,8 +7,7 @@ const questionDB = require('../models/question');
 // Get a specific quiz with all questions
 router.get('/:slug', async (req, res) => {
   try {
-    const quiz = await quizDB.findOne({ slug: req.params.slug })
-      .populate('subject');
+    const quiz = await quizDB.findOne({ slug: req.params.slug });
 
     if (!quiz) {
       return res.status(404).render('error', { message: 'Quiz not found' });
